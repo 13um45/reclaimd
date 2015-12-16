0db8d202552d248327f69ed4844d91216c0dc1e3
13um45 <christian.leumas@icloud.com>
2015-12-16 16:16:57 -0500

    added ability to save an admin.

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -2,4 +2,22 @@ class AdminsController < ApplicationController
   def new
     @admin = Admin.new
   end
+
+  def create
+    @admin = Admin.new admin_params
+
+    if @admin.save
+      redirect_to root_path, notice: "Created Administrator"
+    else
+      render action: 'new'
+    end
+  end
+
+  private
+  def admin_params
+    params.
+    require(:user).
+    permit(:username, :password, :password_confirmation)
+  end
+
 end

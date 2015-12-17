87d214f7d33737b3d56fa74253f7b4cf2adfeb1a
13um45 <christian.leumas@icloud.com>
2015-12-17 16:03:59 -0500

    generated user model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151216233311) do
+ActiveRecord::Schema.define(version: 20151217210327) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,5 +57,14 @@ ActiveRecord::Schema.define(version: 20151216233311) do
     t.string   "name"
     t.integer  "admin_id"
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
 end

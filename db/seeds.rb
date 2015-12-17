e058c4443744cc419dbcf4b96efbd8965c67488f
13um45 <christian.leumas@icloud.com>
2015-12-17 13:57:09 -0500

    added some products to seed file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Product.delete_all
+Product.create! id: 1, name: "shirt", price: 100, quantity: 10, brand: "chrome", material: "cotton",
+description: "loren ipsum"
+Product.create! id: 2, name: "pant", price: 100, quantity: 10, brand: "levis", material: "cotton",
+description: "loren ipsum"
+Product.create! id: 3, name: "shoes", price: 100, quantity: 10, brand: "nike", material: "cotton",
+description: "loren ipsum"

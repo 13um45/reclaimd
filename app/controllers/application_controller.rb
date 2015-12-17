f81edc0dc511b154a5e72bd6177c92962deaf53c
13um45 <christian.leumas@icloud.com>
2015-12-17 18:39:05 -0500

    added current_order method to application controller. added @order_items to carts controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,17 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  helper_method :current_order
   helper_method :current_admin
   helper_method :current_user
+
+  def current_order
+    if !session[:order_id].nil?
+      Order.find(session[:order_id])
+    else
+      Order.new
+    end
+  end
 
   def current_admin
     @current_admin ||= Admin.find_by(id: session[:admin_id])
